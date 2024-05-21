a4461e18d744cd693aa82a40a582da5c1dff555e
Kim Moberg <kimskogfelter@outlook.com>
2024-05-21 09:38:04 +0000

    added favuoriteList and favouriteDetail to views for favourites app

--- favourites/views.py
+++ favourites/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from nerdalert_api.permissions import IsOwnerOrReadOnly
+from favourites.models import Favourite
+from favourites.serializers import FavouriteSerializer
 
-# Create your views here.
+
+class FavouriteList(generics.ListCreateAPIView):
+    """
+    List favourites or create a favourite if user is logged in.
+    """
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = FavouriteSerializer
+    queryset = Favourite.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class FavouriteDetail(generics.RetrieveDestroyAPIView):
+    """
+    Retrieve or delete a favourite by id if you own it.
+    """
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = FavouriteSerializer
+    queryset = Favourite.objects.all()
