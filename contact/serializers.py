4194c275f76f2a512e7f1ff8082ec759fc038314
Kim Moberg <kimskogfelter@outlook.com>
2024-05-22 12:09:30 +0000

    added contactSerializer to the contact app

---
+++ contact/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from .models import ContactModel
+
+class ContactSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ContactModel
+        fields = ['name', 'subject', 'email', 'message', 'created_at']
