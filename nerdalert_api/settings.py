cbccf1667246c043a12465973ec16d56c435a388
Kim Moberg <kimskogfelter@outlook.com>
2024-05-23 13:08:35 +0000

    added allowed_host and client_origin_dev in settings.py

--- nerdalert_api/settings.py
+++ nerdalert_api/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import re
 from pathlib import Path
 import os
 import dj_database_url
@@ -65,7 +65,8 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['8000-kimskogfelt-pp5drfnerda-tagvq47uylz.ws-eu114.gitpod.io', 'localhost', 'https://pp5-nerdalert-drf-api-95b8bddc0859.herokuapp.com/']
+ALLOWED_HOSTS = ['8000-kimskogfelt-pp5drfnerda-tagvq47uylz.ws-eu114.gitpod.io', 'localhost', 'https://pp5-nerdalert-drf-api-95b8bddc0859.herokuapp.com/',
+ os.environ.get('ALLOWED_HOST'),]
 
 CSRF_TRUSTED_ORIGINS = ['https://8000-kimskogfelt-pp5drfnerda-tagvq47uylz.ws-eu114.gitpod.io',]
 
@@ -120,9 +121,7 @@ if 'CLIENT_ORIGIN' in os.environ:
     ]
 
 if 'CLIENT_ORIGIN_DEV' in os.environ:
-    extracted_url = re.match(
-        r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE
-    ).group(0)
+    extracted_url = re.match(r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
     CORS_ALLOWED_ORIGIN_REGEXES = [
         rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
     ]
